b6e62f98d3658fff9c21f54181478c19e0177e24
JanPaJ <patryk4857@gmail.com>
2022-01-03 21:35:30 +0100

    new
    new things

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-alert("Witaj")
+require([
+    "esri/Map",
+    "esri/views/SceneView"
+    // "dijit/form/Button"
+],(Map, SceneView, Button)=>{
+    
+    const map1 = new Map ({
+        basemap: "osm"
+    });
+    const view = new SceneView ({
+        map: map1,
+        container: "mapDiv",
+        zoom: 13,
+        center: [52, 31]
+    });
+
+const myButton = new Button({
+    label:"przycisk",
+    onClick: function () {
+
+    }, "basemap_change");
+})
